7743cea4f731d5e68582ad88c31a32a7eff2b47e
Carl <carlhunt@uw.edu>
2016-04-29 14:16:28 -0700

    4/29 Carl's Checkin 3 deliverable as defined in weekly meeting notes

--- Reviewer.java
+++ Reviewer.java
@@ -1,24 +1,27 @@
+import java.io.File;
 import java.util.List;
 
 
 public class Reviewer extends User {
 
 	public List myManuscripts;
+	public Review[] myReview;
 	
 	public Reviewer(String theName, String theEmail) {
 		super(theName, theEmail);
 	}
 
-	public void submitReview() {
-		
+	public void submitReview(File theReview, int theReviewNum) {
+		myReview[theReviewNum] = new Review(theReview);
 	}
 	
-	public void editReview() {
-		
-	}
+// Note: Commenting this out b/c I don't believe it's required since there is no user story.
+//	public void editReview() {
+//		
+//	}
 	
 	public String toString() {
-		return "Im a Reviewer";
+		return  "Im a Reviewer";
 	}
 
 }
