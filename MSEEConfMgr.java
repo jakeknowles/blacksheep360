632133bd7a7d32cd2f7a0358f4311c6ab59ff7d3
Carl <carlhunt@uw.edu>
2016-04-29 13:40:00 -0700

    4/29 Addedprint statement as a test

--- MSEEConfMgr.java
+++ MSEEConfMgr.java
@@ -1,9 +1,18 @@
 
+// Carl Was Here Test
+/**
+ * 
+ * @author Carl Huntington
+ * @version 4/29/2016
+ */
+
+
 public class MSEEConfMgr {
 
 	//Temporarray Driver to test until we learn Junit.
 	public static void main(String[] theArgs) {
-		
+		MSEEConfMgr x = new MSEEConfMgr();
+		System.out.println(x);
 	}
 
 	public MSEEConfMgr() {
@@ -11,7 +20,7 @@ public class MSEEConfMgr {
 	}
 	
 	public String toString() {
-		return "TEMP";
+		return "I am the conference manager slant temporary test driver";
 	}
 
 	
