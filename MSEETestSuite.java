48308bc3878524244c4ff41610e918178c774a86
Geoffrey Tanay <gctanay@gmail.com>
2016-05-20 13:03:05 -0700

    5/20/2016 added the test suite class

---
+++ MSEETestSuite.java
@@ -0,0 +1,17 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+	AuthorTest.class,
+	ConferenceTest.class,
+	ProgramChairTest.class,
+	ReviewerTest.class,
+	SubProgramChairTest.class,
+	UserTest.class
+})
+
+public class MSEETestSuite {
+
+
+}
